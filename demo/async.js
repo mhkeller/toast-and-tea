7060fca46c0a91da749a87b582c36b2fa98eec71
mhkeller <michael.keller@gmail.com>
2013-12-15 16:31:45 -0500

    refactor, allow for ext scripts

---
+++ demo/async.js
@@ -0,0 +1,30 @@
+var tt         = require('./../toast-and-tea.js');
+
+var data = [
+	{
+		city: 'New York',
+		temp: 27,
+		country: 'USA'
+	},
+	{
+		city: 'Los Angeles',
+		temp: 72,
+		country: 'USA'
+	},
+	{
+		city: 'Paris',
+		temp: 34,
+		country: 'France'
+	},
+	{
+		city: 'Marseille',
+		temp: 43,
+		country: 'France'
+	},
+	{
+		city: 'London',
+		temp: 33,
+		country: 'UK'
+	}
+]
+
